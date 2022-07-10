16c5b7851851b0743d6369c89c15b04aa42fa3db
Kevin Channon <kevinchannon@gmail.com>
2022-07-10 08:21:09 +0100

    Minimize the amount of code in unsafe block

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,9 @@ pub extern "C" fn get_int() -> i32 { 2 }
 pub extern "C" fn get_string(buf: *mut u8, max_len: size_t) -> c_int {
   let out_bytes = "I'm a little teapot".as_bytes();
   let out_size = cmp::min(max_len as usize, out_bytes.len());
+  let terminator_idx = (out_size - 1) as isize;
+        
   unsafe {
-        let terminator_idx = (out_size - 1) as isize;
         
         for i in 0..terminator_idx as isize {
             ptr::write(buf.offset(i), out_bytes[i as usize]);
