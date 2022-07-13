15553830916c080f137cffc07421c9304133d446
Kevin Channon <kevinchannon@gmail.com>
2022-07-13 07:06:05 +0100

    Populate a struct in C from Rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,27 @@
 use libc::{size_t, c_int};
 use std::{cmp, ptr};
 
+///////////////////////////////////////////////////////////////////////////////
+//
+// C-interface parts
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#[repr(C)]
+pub struct User {
+  id: u64,
+}
+
+#[no_mangle]
+pub extern "C" fn get_user(id: u64, user: *mut User) -> bool{
+  if id >= 100 {
+    return false;
+  }
+  
+  unsafe { (*user).id = id; }
+  return true;
+}
+
 #[no_mangle]
 pub extern "C" fn get_int() -> i32 { 2 }
 
@@ -22,6 +43,12 @@ pub extern "C" fn fill_string(buf: *mut u8, max_len: size_t) -> c_int {
     
   out_size as c_int
 }
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Pure Rust, "internal" parts
+//
+///////////////////////////////////////////////////////////////////////////////
 
 fn get_string() -> &'static str {
   "I'm a little teapot"
--- src/main.c
+++ src/main.c
@@ -1,6 +1,18 @@
 #include "rust_interop.h"
 
 #include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+void print_user(bool success, User* user) {
+  if (success) {
+    printf("Found user with ID %lu\n", user->id);
+  } else {
+    printf("FATAL: Failed to find user\n");
+    exit(1);
+  }
+
+}
 
 int main() {
   printf("get_int(): %d\n", get_int());
@@ -12,6 +24,10 @@ int main() {
   len = fill_string(s, 10);
   printf("fill_string(): %s, len=%d\n", s, len);
 
+  User user;
+  print_user(get_user(1, &user), &user);
+  print_user(get_user(100, &user), &user);
+  
   return 0;
 }
 
