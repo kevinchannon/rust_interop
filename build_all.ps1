6295ecb0f35b4c795f81c8405faac080d19e0392
kevin <kevinchannon@gmail.com>
2022-07-17 08:22:15 +0100

    Build exe into Rust target dir on Windows

--- build_all.ps1
+++ build_all.ps1
@@ -2,4 +2,4 @@ cargo build
 
 cbindgen -q --config cbindgen.toml --crate rust_interop --output src/rust_interop.h --lang c
 
-clang .\src\main.c -o interop.exe -L .\target\debug -lrust_interop -lws2_32 -lAdvapi32 -lUserEnv
+clang .\src\main.c -o .\target\debug\interop.exe -L .\target\debug -lrust_interop -lws2_32 -lAdvapi32 -lUserEnv
